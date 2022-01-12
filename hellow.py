32ea1e29b1a6531c147d14ee5d1f224d6261c846
gpatil-test <patilgaurav029@gmail.com>
2022-01-12 17:23:38 +0530

    changed hellow.py

--- hellow.py
+++ hellow.py
@@ -1,1 +1,16 @@
 print("helloo world")
+"186": {
+        "name": "ETM186",
+        "description": "check POST API call for enabling dptsa is working",
+        "tags": [
+
+            "RESTAPI",
+            "PP",
+            "Phase12"
+        ],
+        "suites": [
+            "sanity",
+            "smoke"
+        ]
+    }
+}
