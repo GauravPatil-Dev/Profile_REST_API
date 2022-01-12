fd200c05ba3fcc63df628e2eaf308a934e721566
gpatil-test <patilgaurav029@gmail.com>
2022-01-12 17:30:01 +0530

    changed hello.py

--- hellow.py
+++ hellow.py
@@ -1,1 +1,16 @@
 print("helloo world")
+"180": {
+        "name": "ETM180",
+        "description": "chec POST API call for enabling dptsa is working",
+        "tags": [
+
+            "RESTAPI",
+            "PP",
+            "Phase12"
+        ],
+        "suites": [
+            "sanity",
+            "smoke"
+        ]
+    }
+}
